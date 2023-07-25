401de64a8e9f92d5ba7be3ba80aa56079e614a91
Azula Dev <caligeekweb@gmail.com>
2023-07-25 10:46:39 -0500

    commit

--- app/src/main/java/www/colegiolancaster/com/CalificacionesActivity.java
+++ app/src/main/java/www/colegiolancaster/com/CalificacionesActivity.java
@@ -2,7 +2,9 @@ package www.colegiolancaster.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CalificacionesActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CalificacionesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calificaciones);
+    }
+    public void logoutNotas(View view){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
